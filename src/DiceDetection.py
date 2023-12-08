fe04aeaa30a08e42bc145f9a343d6e70a5bafd33
Luiza Valezim Augusto Pinto <62949391+LuizaValezim@users.noreply.github.com>
2023-12-07 21:58:52 -0300

    Adicionando funções síncronas e assíncronas

--- src/DiceDetection.py
+++ src/DiceDetection.py
@@ -14,14 +14,8 @@ class DiceDetecion:
         self.params.minInertiaRatio = 0.6
         self.detector = cv2.SimpleBlobDetector_create(self.params)
 
-    def get_blobs(self, frame):
+    async def get_blobs(self, frame):
         """this function glaubers
-
-        Args:
-            frame (_type_): _description_
-
-        Returns:
-            _type_: _description_
         """
         frame_blurred = cv2.medianBlur(frame, 7)
         frame_gray = cv2.cvtColor(frame_blurred, cv2.COLOR_BGR2GRAY)
@@ -29,8 +23,9 @@ class DiceDetecion:
 
         return blobs
 
-    def get_dice_from_blobs(self, blobs):
-        
+    async def get_dice_from_blobs(self, blobs):
+        """this function piriri pororo
+        """
         X = []
         for b in blobs:
             pos = b.pt
@@ -42,33 +37,26 @@ class DiceDetecion:
 
         if len(X) > 0:
             clustering = cluster.DBSCAN(eps=50, min_samples=1).fit(X)
-
             num_dice = max(clustering.labels_) + 1
-
             dice = []
 
             for i in range(num_dice):
                 X_dice = X[clustering.labels_ == i]
-
                 centroid_dice = np.mean(X_dice, axis=0)
-
                 dice.append([len(X_dice), *centroid_dice])
 
             return dice
-
         else:
             return []
 
-    def overlay_info(self, frame, dice, blobs):
+    async def overlay_info(self, frame, dice, blobs):
         for b in blobs:
             pos = b.pt
             r = b.size / 2
-
             cv2.circle(frame, (int(pos[0]), int(pos[1])), int(r), (255, 0, 0), 2)
 
         for d in dice:
             textsize = cv2.getTextSize(str(d[0]), cv2.FONT_HERSHEY_PLAIN, 3, 2)[0]
-
             cv2.putText(
                 frame,
                 str(d[0]),
--- src/main.py
+++ src/main.py
@@ -1,24 +1,33 @@
 from DiceDetection import DiceDetecion
+import cv2
+import numpy as np
 from fastapi import FastAPI
 import uvicorn
-import cv2
+from sklearn.cluster import DBSCAN
+import asyncio
 
 app = FastAPI()
 
 @app.get("/dice-detection")
 @app.route('/')
 
-def main(*args):
+async def main():
     captured = cv2.VideoCapture(0)
     dice_detector = DiceDetecion()
 
     while True:
         ret, frame = captured.read()
 
-        blobs = dice_detector.get_blobs(frame)
-        dice = dice_detector.get_dice_from_blobs(blobs)
-        dice_detector.overlay_info(frame, dice, blobs)
+        if not ret:
+            print("Error reading frame from the camera")
+            break
+
+        blobs = await dice_detector.get_blobs(frame)
+        dice = await dice_detector.get_dice_from_blobs(blobs)
+        await dice_detector.overlay_info(frame, dice, blobs)
+
         print(dice)
+
         cv2.imshow("frame", frame)
 
         res = cv2.waitKey(1)
@@ -27,4 +36,7 @@ def main(*args):
             break
 
 if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
     uvicorn.run(app, host="127.0.0.1", port=8000)
